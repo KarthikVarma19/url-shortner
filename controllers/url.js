f8464d17b4b72752311c09886fa724073dec764a
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-02-15 18:15:58 +0530

    added server side rendering(ssr) of homepage i.e staticroute using ejs

--- controllers/url.js
+++ controllers/url.js
@@ -1,41 +1,52 @@
-const {nanoid} = require("nanoid")
-const URL = require('../models/url')
-const validUrl = require('valid-url');
-const dotenv = require('dotenv');
+const { nanoid } = require("nanoid");
+const URL = require("../models/url");
+const validUrl = require("valid-url");
+const dotenv = require("dotenv");
 
+async function handleGenerateNewShortURL(req, res) {
+  if (!req.body.original_url) {
+    return res.render("home", {
+      error: "url is required",
+    });
+    // return res.status(404).({error: "url is required"});
+  }
 
-async function handleGenerateNewShortURL(req, res){
+  const originalURL = req.body.original_url;
 
-    if(!req.body.original_url){
-        return res.status(404).json({error: "url is required"});
+  if (validUrl.isWebUri(originalURL)) {
+    const customUrl = req.body.custom_url;
+    dotenv.config();
+    let shortId = nanoid(Number(process.env.SHORT_URL_LENGTH));
+    if (customUrl) {
+      shortId = customUrl;
     }
-
-    const originalURL = req.body.original_url;
-    
-    if(validUrl.isWebUri(originalURL)){
-        const customUrl = req.body.custom_url;
-        dotenv.config();
-        let shortId = nanoid(Number(process.env.SHORT_URL_LENGTH));
-        if(customUrl){
-            shortId = customUrl;
-        }
-        const exists = await URL.findOne({shortId: shortId});
-        if(exists){
-            return res.json({id: exists.shortId, status: "already exists so redirected"});
-        }
-        await URL.create({
-            shortId: shortId,
-            redirectURL: originalURL,
-            visitHistory: [],
-        });
-    
-        return res.json({id: shortId});
+    const exists = await URL.findOne({ shortId: shortId });
+    if (exists) {
+      return res.render("home", {
+        id: exists.shortId,
+      });
+      /*
+        return res.json({
+        id: exists.shortId,
+        status: "already exists so redirected",
+      });
+      */
     }
-    return res.status(404).json({error: "url is incorrect"});
-    
+    await URL.create({
+      shortId: shortId,
+      redirectURL: originalURL,
+      visitHistory: [],
+    });
+
+    return res.render("home", {
+      id: shortId,
+    });
+    //return res.json({id: shortId});
+  }
+  return res.render("home", { error: "url is incorrect" });
+  //return res.status(404).json({error: "url is incorrect"});
 }
-
 
 module.exports = {
-    handleGenerateNewShortURL,
-}
+  handleGenerateNewShortURL,
+};
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const urlRoute = require('./routes/url')
-const shortUrlRoute = require('./routes/shortUrl')
-const analyticsRoute = require('./routes/analyticsRoute')
-const {connectToMongoDb} = require('./connect')
-const dotenv = require('dotenv');
+const express = require("express");
+const urlRoute = require("./routes/url");
+const shortUrlRoute = require("./routes/shortUrl");
+const analyticsRoute = require("./routes/analyticsRoute");
+const staticRoute = require("./routes/staticRouter");
+const { connectToMongoDb } = require("./connect");
+const dotenv = require("dotenv");
+const URL = require("./models/url");
+const path = require("path");
 
 const app = express();
 const PORT = 8001;
@@ -11,17 +14,23 @@ const PORT = 8001;
 dotenv.config();
 
 const URI = process.env.URI;
-connectToMongoDb(URI).then(()=>{
-    console.log("MongoDB Connected");
+connectToMongoDb(URI).then(() => {
+  console.log("MongoDB Connected");
 });
 
-//app.use(express.urlencoded({extended: false}));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve('./views'));
+app.use(express.urlencoded({extended: false}));
+
+//Home Page
+app.use("/", staticRoute);
+
 app.use(express.json());
 app.use("/url", urlRoute);
 app.use("/", shortUrlRoute);
-app.use("/analytics", analyticsRoute)
-
+app.use("/analytics", analyticsRoute);
 
 app.listen(PORT, () => {
-    console.log(`Server Started Running at ${PORT}`);
-})
+  console.log(`Server Started Running at ${PORT}`);
+});
